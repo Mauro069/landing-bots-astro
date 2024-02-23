1971b8811152dbc8d19ab59fdac407ca20631077
Mauro Vera <Maurovera069@gmail.com>
2024-02-23 10:46:55 -0300

    Navbar

---
+++ src/utils/navLinks.ts
@@ -0,0 +1,18 @@
+export const navLinks = [
+  {
+    title: "Home",
+    href: "#home",
+  },
+  {
+    title: "My Robots",
+    href: "#robots",
+  },
+  {
+    title: "My Services",
+    href: "#services",
+  },
+  {
+    title: "Faq",
+    href: "#faq",
+  },
+];
