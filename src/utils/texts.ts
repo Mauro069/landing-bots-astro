489fa4418df6a62caa803519d017e5867c774f6d
Mauro Vera <Maurovera069@gmail.com>
2024-02-23 19:14:22 -0300

    Cards

---
+++ src/utils/texts.ts
@@ -0,0 +1,26 @@
+export const cards = {
+  "1": {
+    img: "/assets/1.png",
+    title: "Strategy based on Smart Money concepts",
+    description:
+      "Discover an automated trading strategy that is based on the principles of Smart Money. Our robots use advanced algorithms that analyze and operate based on intelligent market movements, giving you a unique competitive advantage.",
+  },
+  "2": {
+    img: "/assets/2.png",
+    title: "Automated Take Profit",
+    description:
+      "With our trading robots, you will not have to worry about making trade closing decisions. The Take Profit feature is built in automatically, allowing you to set your profit goals and let the robot take care of the rest, thus maximizing your results.",
+  },
+  "3": {
+    img: "/assets/3.png",
+    title: "Automated Stop Loss and Trailing Stop",
+    description:
+      "Risk management is essential in trading, which is why our robots include automatic Stop Loss and Trailing Stop functions. This allows you to protect your trades against adverse market movements and ensure profits while minimizing losses, all in a fully automated manner.",
+  },
+  "4": {
+    img: "/assets/4.png",
+    title: "No need for manual adjustments",
+    description:
+      "With our robots, you don't need to make complicated manual adjustments. Everything is designed to work automatically and efficiently. From risk management to trade execution, our products are optimized to give you consistent results without the need for human intervention.",
+  },
+};
