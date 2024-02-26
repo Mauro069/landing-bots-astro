e00d902841edc7b46b55f5b1304469fb5e2b1e58
Mauro Vera <Maurovera069@gmail.com>
2024-02-26 15:18:38 -0300

    MyRobots section

--- src/utils/texts.ts
+++ src/utils/texts.ts
@@ -24,3 +24,30 @@ export const cards = {
       "With our robots, you don't need to make complicated manual adjustments. Everything is designed to work automatically and efficiently. From risk management to trade execution, our products are optimized to give you consistent results without the need for human intervention.",
   },
 };
+
+export const robots = {
+  "1": {
+    iconUrl: "/assets/diamond.png",
+    color: "#00E2F5",
+    price: 1100,
+    title: "Diamond-V5",
+    description:
+      "Monthly profitability 3-5% <br/> Customization level - low <br/> Multi-pairs support - no <br/> Strategy - Smart money <br/> works on any broker - yes <br/> professional risk management <br/> winrate 60-70% <br/> Martingala - no <br/> grid - no <br/> hedge - no <br/> license - no",
+  },
+  "2": {
+    iconUrl: "/assets/esmerald.png",
+    color: "#01D300",
+    price: 2500,
+    title: "Emerald-V5",
+    description:
+      "Monthly profitability 5-8% <br/> Customization level - half <br/> Multi-pairs support - no <br/> Strategy - Smart money <br/> works on any broker - yes <br/> professional risk management <br/> winrate 75-80% <br/> Martingala - no <br/> grid - no <br/> hedge - no <br/> license - no",
+  },
+  "3": {
+    iconUrl: "/assets/zafiro.png",
+    color: "#E807DF",
+    price: 3500,
+    title: "Zafiro-V5",
+    description:
+      "Monthly profitability 3-5% <br/> Customization level - high <br/> Multi-pairs support - yes <br/> Strategy - Smart money <br/> works on any broker - yes <br/> professional risk management <br/> winrate 80-90% <br/> Martingala - no <br/> grid - no <br/> hedge - no <br/> license - no",
+  },
+};
