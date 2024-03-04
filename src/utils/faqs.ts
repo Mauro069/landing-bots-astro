1cd8851e6d138ca1ddec759cf5d9be7e207f648c
Mauro Vera <Maurovera069@gmail.com>
2024-03-04 14:20:11 -0300

    Agregue todas las preguntas e hice responsive

--- src/utils/faqs.ts
+++ src/utils/faqs.ts
@@ -21,7 +21,95 @@ export const faqs = [
     a: "I accept payments in cryptocurrencies and credit and debit cards.",
   },
   {
-    q: "",
-    a: "",
+    q: "What type of support is offered after purchasing a Frank product?",
+    a: "I offer personalized 1 on 1 support for each customer, including technical assistance and help with any questions or problems related to my products.",
+  },
+  {
+    q: "What is the return policy for Frank's products?",
+    a: "I offer a 30 day return period if you are not satisfied with your purchase.",
+  },
+  {
+    q: "Do Frank's products work on all trading platforms?",
+    a: "Yes, my robots are compatible with Metatrader 4 and Metatrader 5, the most common trading platforms.",
+  },
+  {
+    q: "How are Frank's products delivered after purchase?",
+    a: "Products are delivered immediately after payment is made, providing the chosen robot file ready to use.",
+  },
+  {
+    q: "Which currency pair is recommended for trading with Frank's robots?",
+    a: "Although my robots can trade any currency pair, I recommend the US30 index for the best results.",
+  },
+  {
+    q: "Do Frank's products include future updates?",
+    a: "Yes, all purchases include future updates to Frank's products at no additional cost.",
+  },
+  {
+    q: "Do Frank's bots have built-in news filters?",
+    a: "Yes, all my robots include news filters to avoid trading during times of high volatility.",
+  },
+  {
+    q: "Can I use Frank's products on different size beads?",
+    a: "Yes, my robots are fully customizable and can adapt to any account size.",
+  },
+  {
+    q: "What is the level of risk associated with using Frank's products?",
+    a: "My robots are designed with customizable risk management to fit each trader's individual preferences.",
+  },
+  {
+    q: "What type of analysis supports Frank's products?",
+    a: "My products are backed by realistic analyzes and strategies based on solid Smart Money concepts.",
+  },
+  {
+    q: "Are results guaranteed when using Frank's products?",
+    a: "Although I cannot guarantee specific results, my robots are designed to deliver solid and profitable performance in the financial market.",
+  },
+  {
+    q: "What is the difference between the different product packages offered by Frank?",
+    a: "I offer a variety of packages with different features and prices to suit each operator's individual needs.",
+  },
+  {
+    q: "How do Frank's robots adjust to different runners?",
+    a: "My robots automatically adjust to different riders to ensure optimal performance on each platform.",
+  },
+  {
+    q: "Are Frank products backed by any type of warranty?",
+    a: "I offer a 100% satisfaction guarantee and am committed to providing ongoing support and assistance to my customers.",
+  },
+  {
+    q: "What type of setup is required to use Frank's products?",
+    a: "My products come ready to use, with no complicated setup required. You only have to worry about choosing your risk management preferences.",
+  },
+  {
+    q: "What is the average performance of Frank's robots?",
+    a: "Performance may vary, but my robots have an average monthly performance ranging from 5% to 21%, depending on the product.",
+  },
+  {
+    q: "Are verified performance metrics provided for Frank's products?",
+    a: "Yes, I provide verified performance metrics through services like MyFxBook and FTMO Metrix to support the results of my products.",
+  },
+  {
+    q: "What type of strategies are used in Frank's robots?",
+    a: "My robots are based on Smart Money strategies and do not use high-risk trading methods such as Martingale or HFT.",
+  },
+  {
+    q: "Is training or education provided on the use of Frank's products?",
+    a: "I offer personalized support to help you understand and use my products effectively.",
+  },
+  {
+    q: "Do Frank's products have any usage limitations or restrictions?",
+    a: "My products are designed to operate in any market environment, but it is recommended to follow appropriate risk management practices.",
+  },
+  {
+    q: "How can I get more information about Frank's products?",
+    a: "You can obtain more information about my products by visiting my website or contacting me directly through my contact channels.",
+  },
+  {
+    q: "Are trial versions of Frank's products provided?",
+    a: "No, to avoid fraud and maintain the security of my products, I do not offer trial versions.",
+  },
+  {
+    q: "Do Frank's products have any license limitations or usage restrictions?",
+    a: "Each customer receives a unique version of the product to avoid duplication and security issues. In the event that a duplication is detected, the robot automatically cancels its effectiveness and performance.",
   },
 ];
