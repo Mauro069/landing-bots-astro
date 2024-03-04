3bde755fa5d6f7da0f3bcf7b6be982ec281b30d7
Mauro Vera <Maurovera069@gmail.com>
2024-03-04 14:10:22 -0300

    Faqs

---
+++ src/components/Faqs/index.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import styles from './Faqs.module.css'
+import { faqs } from '../../utils/faqs'
+
+export const Faqs = () => {
+  const [questionSelected, setQuestionSelected] = useState(faqs[0])
+
+  const toggleQuestion = ({ q, a }) => {
+    if (q === questionSelected?.q) {
+      setQuestionSelected(null)
+      return
+    }
+
+    setQuestionSelected({ q, a })
+  }
+
+  return (
+    <section id='faq' className={styles['faqs-container']}>
+      <h3>Frequently Asked Questions</h3>
+
+      <div className={styles.faqs}>
+        {faqs.map(({ q, a }) => {
+          const isThis = q === questionSelected?.q
+
+          return (
+            <div
+              className={`${styles.faq} ${isThis ? styles.active : ''}`}
+              onClick={() => toggleQuestion({ q, a })}
+            >
+              <div className={styles.question}>
+                <span>{q}</span>
+
+                <p>{isThis ? '+' : '-'}</p>
+              </div>
+
+              <div className={styles.answer}>
+                <span>{a}</span>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </section>
+  )
+}
---
+++ src/utils/faqs.ts
@@ -0,0 +1,27 @@
+export const faqs = [
+  {
+    q: "What type of products does Frank offer on his website?",
+    a: "I offer the only trading robots with real strategies and risk management as a percentage of the entire market.",
+  },
+  {
+    q: "How can I access Frank's products?",
+    a: "Going to the “products” section you will find Frank's robots ready to be delivered.",
+  },
+
+  {
+    q: "What differentiates Frank's trading robots from others available on the market?",
+    a: "My robots are based on solid strategies backed by Smart Money concepts and are designed to provide consistent and realistic results.",
+  },
+  {
+    q: "What is the pricing policy for Frank's products?",
+    a: "My products are competitively priced and offered with special discounts when paying with USDT. These prices vary depending on the product and the payment method selected.",
+  },
+  {
+    q: "What payment methods are accepted to purchase Frank's products?",
+    a: "I accept payments in cryptocurrencies and credit and debit cards.",
+  },
+  {
+    q: "",
+    a: "",
+  },
+];
