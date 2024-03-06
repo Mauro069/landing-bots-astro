1cd29d6a7a725aa761f1172e01eced4fecd1cfed
Mauro Vera <Maurovera069@gmail.com>
2024-03-06 12:54:47 -0300

    Desinstale analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
-import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
-import vercel from '@astrojs/vercel/serverless'
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  })
-})
+  integrations: [react()]
+});
