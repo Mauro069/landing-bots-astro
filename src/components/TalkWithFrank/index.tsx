94db0a95ae6aa4aa42d81c108bf6ed99a27e3121
Mauro069 <maurovera069@gmail.com>
2024-04-10 16:18:47 -0300

    FIX: Elimine links a whatsapp

--- src/components/TalkWithFrank/index.tsx
+++ src/components/TalkWithFrank/index.tsx
@@ -1,31 +1,23 @@
-import { useState } from 'react'
-import { Faqs } from '../../components/Faqs'
-import styles from './TalkWithFrank.module.css'
+import { useState } from "react";
+import styles from "./TalkWithFrank.module.css";
 
 export const TalkWithFrank = () => {
-  const [show, setShow] = useState(true)
+  const [show, setShow] = useState(true);
 
   return (
-    <section id='contact' className={`section ${styles['talk-with-frank']}`}>
+    <section id="contact" className={`section ${styles["talk-with-frank"]}`}>
       <h3 className={styles.title}>Contact</h3>
 
       {show ? (
         <button className={styles.button} onClick={() => setShow(false)}>
-          <img className={styles.img} src='/assets/cabeza.png' />
+          <img className={styles.img} src="/assets/cabeza.png" />
           <p className={styles.text}>Talk with Frank</p>
         </button>
       ) : (
         <div className={styles.socials}>
           <a
-            href='https://wa.me/message/53WVGAKRZ7OSB1'
-            target='_blank'
-            className={styles.wsp}
-          >
-            Whatsapp
-          </a>
-          <a
-            href='https://t.me/FrankBOTsMaker'
-            target='_blank'
+            href="https://t.me/FrankBOTsMaker"
+            target="_blank"
             className={styles.tl}
           >
             Telegram
@@ -33,5 +25,5 @@ export const TalkWithFrank = () => {
         </div>
       )}
     </section>
-  )
-}
+  );
+};
